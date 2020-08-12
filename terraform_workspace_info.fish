89cf0c1afd675231c0e8ef07969356af58ae71e1
Phong Nguyen <nhphong1406@gmail.com>
2020-08-12 17:11:36 +0700

    Update terraform_workspace_info

--- terraform_workspace_info.fish
+++ terraform_workspace_info.fish
@@ -1,6 +1,7 @@
 function terraform_workspace_info
     if test -d .terraform
-        echo -n -s ' (' (terraform workspace show 2>/dev/null) ')'
+        set -l workspace (terraform workspace show 2^/dev/null)
+        test -n "$workspace"; and echo -n -s ' (' $workspace ')'
     end
 end
 
