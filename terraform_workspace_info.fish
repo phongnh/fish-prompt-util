98bc8ffcf999d38d942fa461b8079a5471d8f2da
Phong Nguyen <nhphong1406@gmail.com>
2020-10-26 22:33:31 +0700

    Check if terraform command exists

--- terraform_workspace_info.fish
+++ terraform_workspace_info.fish
@@ -1,5 +1,5 @@
 function terraform_workspace_info
-    if test -d .terraform
+    if test -d .terraform ;and type terraform >/dev/null 2>&1
         set -l workspace (terraform workspace show 2^/dev/null)
         test -n "$workspace"; and echo -n -s ' (' $workspace ')'
     end
